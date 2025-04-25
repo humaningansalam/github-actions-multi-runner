7efd85a05a7887cd2e36d9fef0179b0b2d1622ad
sanghun9634 <sanghun9634@gmail.com>
2025-04-25 15:20:26 +0900

    chore: add start.sh

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+GITHUB_URL=$GITHUB_URL
+GITHUB_TOKEN=$GITHUB_TOKEN
+RUNNER_NAME=${RUNNER_NAME:-$(hostname)}
+RUNNER_WORK_DIR=${RUNNER_WORK_DIR:-_work}
+RUNNER_LABELS=${RUNNER_LABELS:-default}
+
+# If the runner has already been configured, skip the config step
+if [ ! -f ".runner" ]; then
+    # Configure the runner
+    ./config.sh \
+        --url "${GITHUB_URL}" \
+        --token "${GITHUB_TOKEN}" \
+        --name "${RUNNER_NAME}" \
+        --work "${RUNNER_WORK_DIR}" \
+        --labels "${RUNNER_LABELS}" \
+        --unattended \
+        --replace
+fi
+
+# Cleanup any existing _diag directory
+rm -rf "_diag" || true
+
+# Start the runner
+exec ./run.sh
